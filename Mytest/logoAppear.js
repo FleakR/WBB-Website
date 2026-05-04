d7251333132dc617a4f3c0e66cc70506eddd481a
FleakR <J-Parry@hotmail.com>
2026-05-04 18:34:23 +0100

    Delete Mytest directory

--- Mytest/logoAppear.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function () {
-  const logo = document.querySelector(".mainLogo");
-
-  if (!logo) return;
-
-  setTimeout(function () {
-    logo.classList.add("show");
-  }, 1000);
-});
